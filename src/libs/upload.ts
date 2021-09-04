9e45399d9a5783de349b233383f5a64ab299ae1c
kokily <kokily@naver.com>
2021-09-04 13:27:43 +0900

    upload modify

--- src/libs/upload.ts
+++ src/libs/upload.ts
@@ -62,7 +62,8 @@ upload.post('/', async (ctx: Context, next: Next) => {
   if (ctx.request.files) {
     const file = ctx.request.files.file;
 
-    const { key, url } = await uploadImage(file[0]);
+    // @ts-ignore
+    const { key, url } = await uploadImage(file);
 
     ctx.body = { key, url };
   } else {
